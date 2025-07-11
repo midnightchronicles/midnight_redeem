c04c18bc46aa3ef3edea19cd8f97ce187b77a56c
Midnight Chronicles RP <leesoldon26@icloud.com>
2025-07-11 14:47:24 +0100

    version checker

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 "yes"
+version '1.0.1'
 
 description 'Redeem System'
 
 shared_scripts {
     '@ox_lib/init.lua',
+    'config.lua',
 }
 
 server_scripts {
--- server.lua
+++ server.lua
@@ -3,6 +3,73 @@ local Bridge = exports['community_bridge']:Bridge()
 
 local webhookUrl = ''
 
+local function versionCheck(resource, repository, paid)
+    local currentVersion = GetResourceMetadata(resource, 'version', 0)
+    if not currentVersion then
+        print("^4Please contact ^0[^5Midnight Chronicles^0]^4 for support and reference this error:^0 ".."^3SS_Util.VersionCheck^0, ^1Can't find current resource version for '%s'^0[^3"..resource.."^0]")
+        return
+    end
+
+    SetTimeout(1000, function()
+        PerformHttpRequest(('https://api.github.com/repos/%s/releases/latest'):format(repository), function(status, response)
+            if status ~= 200 then
+                if status == 403 or status == 429 then
+                    print("[^1Update check failed for^0] [^3"..resource.."^0] [^3Git API Limitations^0]\n[^4You may still get this error for a while when restarting the script or server.^0]")
+                else
+                    print("[^3RUN^0] [^1ERROR^0] [^4Reference^0] [^3VersionCheck^0]\n[^1Check^0] [^3"..repository.."^0] [^4Status^0] [^3"..status.."^0]")
+                end
+                return
+            end
+
+            response = json.decode(response)
+            if response.prerelease then return end
+
+            local latestVersion = response.tag_name:match('%d+%.%d+%.%d+') or response.tag_name:match('%d+%.%d+')
+            if not latestVersion then
+                return
+            elseif latestVersion == currentVersion then
+                print("[^5Midnight_Redeem^0] [^2Is up to date^0] [^4Your version^0] [^3"..currentVersion.."^0] [^4Latest Version^0] [^3"..latestVersion.."^0]")
+            else
+                local cv = { string.strsplit('.', currentVersion) }
+                local lv = { string.strsplit('.', latestVersion) }
+
+                local maxParts = math.min(#cv, #lv)
+                for i = 1, maxParts do
+                    local current, minimum = tonumber(cv[i] or 0), tonumber(lv[i] or 0)
+                    if i == maxParts then
+                        if (#cv > i and current == minimum and tonumber(cv[i+1] or 0) > tonumber(lv[i+1] or 0)) or (#cv >= i and current > minimum) then
+                            if paid then
+                                print("[^5Midnight_Redeem^0] [^4is newer than expected^0] [^4Your version^0] [^3"..currentVersion.."^0] [^4Latest Version^0] [^3"..latestVersion.."^0]\n[^1Please downgrade to latest release for^0] [^3"..resource.."^0] [^1through the cfx portal]")
+                                break
+                            else
+                                print("[^5Midnight_Redeem^0] [^4is newer than expected^0] [^4Your version^0] [^3"..currentVersion.."^0] [^4Latest Version^0] [^3"..latestVersion.."^0]\n[^1Please downgrade to latest release here: https://github.com/midnight_run/releases/latest^0]")
+                                break
+                            end
+                        end
+                    end
+                    if current ~= minimum then
+                        if current < minimum then
+                            if not paid then
+                                print("[^5Midnight_Redeem^0] [^4is outdated^0] [^4Your version^0] [^3"..currentVersion.."^0] [^4Latest Version^0] [^3"..latestVersion.."^0]\n[^1Please update^0] [^3"..resource.."^0] [^1here:^0]\n[^5https://github.com/midnight_run/releases/latest^0]")
+                                break
+                            else
+                                print("[^5Midnight_Redeem^0] [^4is outdated^0] [^4Your version^0] [^3"..currentVersion.."^0] [^4Latest Version^0] [^3"..latestVersion.."^0]\n[^1Please update^0] [^3"..resource.."^0] [^1through the cfx portal^0]")
+                                break
+                            end
+                        end
+                    end
+                end
+            end
+        end, 'GET')
+    end)
+end
+
+AddEventHandler('onResourceStart', function(resource)
+    if resource == "midnight_redeem" then
+        versionCheck(resource,"midnightchronicles/midnight_redeem_version",false)
+    end
+end)
+
 function DebugPrint(message)
     if Config.Debug then
         print("[Midnight_Redeem] " .. message)
