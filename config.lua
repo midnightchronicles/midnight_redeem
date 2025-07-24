15b680e87ea88b341219cdddd6d9d27bea5cc5ec
Midnight Chronicles RP <leesoldon26@icloud.com>
2025-07-24 22:47:02 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,10 @@
 Config = {}
 
-Config.Debug = false
+Config.Debug = true
 
 Config.GenerateCommand = "generatecode"
 Config.RedeemCommand = "redeemcode"
+Config.Framework = "qb" -- 'qb' for qbcore or qbox ---- 'esx' for esx framework
+
 
 return Config
