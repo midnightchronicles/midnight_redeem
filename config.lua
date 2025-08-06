d0a4c0a49adb5d09b306f3e84dc14e4d5d93656c
Midnight Chronicles RP <leesoldon26@icloud.com>
2025-08-06 22:24:08 +0100

    V.1.3
    locale support added for different translations

--- config.lua
+++ config.lua
@@ -1,9 +1,10 @@
 Config = {}
 
-Config.Debug = false
+Config.Debug = true
 
+Config.Framework = 'qb'
 Config.AdminCommand = "adminredeem"
 Config.RedeemCommand = "redeemcode"
-
+Config.Language = 'en' -- 'en' english | ''  | ''  |
 
 return Config
