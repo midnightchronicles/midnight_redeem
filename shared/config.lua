aef9170d67f369938f85782a68765c530e4f8530
Midnight Chronicles RP <leesoldon26@icloud.com>
2025-08-25 15:31:35 +0100

    Update config.lua
    Config.mincustomchar added

--- shared/config.lua
+++ shared/config.lua
@@ -3,7 +3,8 @@ Config = {}
 Config.Debug = true
 
 Config.Framework = 'qb'
-Config.AdminCommand = "adminredeem"
-Config.RedeemCommand = "redeemcode"
+Config.AdminCommand = "adminredeem" -- admin menu
+Config.RedeemCommand = "redeemcode" -- user redeeem command
+Config.mincustomchar = 8 -- minimum character length for custom code set to false or 0 if you want to disable
 
 return Config
